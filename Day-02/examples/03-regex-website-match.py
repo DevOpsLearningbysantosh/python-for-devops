1f8b22eae5edcc843ee296987031b7622f4610f1
Santosh <126384863+CloudSantosh@users.noreply.github.com>
2023-10-27 11:20:58 +0000

    Changes in the file Day-02/examples/03-regex-website-match.py

--- Day-02/examples/03-regex-website-match.py
+++ Day-02/examples/03-regex-website-match.py
@@ -1,20 +1,22 @@
+"""Module providing a function printing python version."""
 import re
 import requests
 
 # Define the URL of the website you want to extract text from
-website_url = "https://edition.cnn.com/world"  # Replace with the URL of the website
+website = "https://www.reuters.com"  # Replace with the URL of the website
 
 # Send an HTTP GET request to the website
-response = requests.get(website_url)
+response = requests.get(website, timeout=5)
 
 # Check if the request was successful (status code 200)
 if response.status_code == 200:
     # Define a regular expression pattern to match famous text
     # This is just an example pattern; you should customize it for your use case
-    famous_text_pattern = r"USA"
+    text_sample=input()
+    PATTERN = rf"{text_sample}"
 
     # Extract text from the HTML content using the regular expression
-    famous_text_matches = re.findall(famous_text_pattern, response.text)
+    famous_text_matches = re.findall(PATTERN, response.text)
 
     # Display the extracted famous text
     if famous_text_matches:
